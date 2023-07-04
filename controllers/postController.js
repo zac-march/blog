e0ee0a4e105ec90af34dfb820da754d375d00228
Zachariah March <121649297+zac-march@users.noreply.github.com>
2023-07-05 08:07:12 +1000

    Add Post and post list endpoint

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,14 +1,33 @@
 const Post = require("../models/post");
+const User = require("../models/user");
 const asyncHandler = require("express-async-handler");
 
 // Display list of all posts.
 exports.post_list = asyncHandler(async (req, res, next) => {
-  res.json({ message: "NOT IMPLEMENTED: post list" });
+  let posts = await Post.find({}).limit(5).sort("-dateAdded");
+  return res.json(posts);
 });
 
 // Display detail page for a specific post.
 exports.post_detail = asyncHandler(async (req, res, next) => {
-  res.json({ message: `NOT IMPLEMENTED: post detail: ${req.params.id}` });
+  try {
+    let post = await Post.findOne({ _id: req.params.id }).populate(
+      "user",
+      "username"
+    );
+    if (!post) {
+      return res.status(404).json({ error: "Post not found" });
+    }
+
+    return res.json({
+      body: post.body,
+      title: post.title,
+      dateAdded: post.dateAdded,
+      author: post.user.username,
+    });
+  } catch (error) {
+    next(error);
+  }
 });
 
 // Handle post create on POST.
